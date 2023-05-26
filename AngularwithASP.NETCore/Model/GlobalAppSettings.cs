f86d7c1fb5808ca529c600791244a6bd71309dc3
Nithya <nithya.gopal@syncfusion.com>
2023-05-26 11:06:47 +0530

    309294-Configure-embedConfigjson-instead-of-EmbedProperties-in-angularwithcore

---
+++ AngularwithASP.NETCore/Model/GlobalAppSettings.cs
@@ -0,0 +1,13 @@
+﻿using AngularwithASP.NETCore.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AngularwithASP.NETCore.Model
+{
+    public class GlobalAppSettings
+    {
+        public static EmbedDetails EmbedDetails { get; set; }
+    }
+}
