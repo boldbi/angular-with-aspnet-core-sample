adb741dbf2b05ed922da1f77d78b713e9651146e
Nithya <nithya.gopal@syncfusion.com>
2023-07-12 14:09:49 +0530

    319324-Correction-in-angular-with-core-sample-in-GitHub

--- Angular-with-ASP.NETCore/ClientApp/src/app/app.component.ts
+++ Angular-with-ASP.NETCore/ClientApp/src/app/app.component.ts
@@ -9,7 +9,7 @@ import { appService } from './app.service';
   export class AppComponent {
 
     //ASP.NET Core application would be run on https://localhost:5001;http://localhost:5000, which needs to be set as `apiHost`
-    public apiHost="http://localhost:5210/";
+    public apiHost="https://localhost:7051/";
 
     //Url of the GetDetails action in ValuesController of the ASP.NET Core application
     public authorizationUrl= "api/boldbiembed/authorizationserver";
